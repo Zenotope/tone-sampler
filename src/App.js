97221187261bf10b1ed831349d7979189771b641
Zenotope <zaak.kerstetter@gmail.com>
2022-08-04 09:48:54 -0600

    first edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
+import * as Tone from 'tone'
 import './App.css';
+
+// document.querySelector('button')?.addEventListener('click', async () => {
+// 	await Tone.start()
+// 	console.log('audio is ready')
+// })
+
+const sampler = new Tone.Sampler({
+	urls: {
+    A1: "A1.mp3",
+		A2: "A2.mp3",
+    
+	},
+	baseUrl: "https://tonejs.github.io/audio/casio/",
+	}).toDestination();
+
+  Tone.loaded().then(() => {
+    sampler.triggerAttackRelease(["C3", "Db3", "D3", "D3"], 0.5);
+  })
+
+  // "C3": "C3.mp3",
+  //   "Db3": "C#3.mp3",
+  //   "D3": "D3.mp3",
+  //   "Eb3": "D#3.aif",
+  //   "E3": "E3.aif",
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>test</p>
+    <button>start</button>
     </div>
   );
 }
