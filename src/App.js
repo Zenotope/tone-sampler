f97f1a059bbff7f78017377014a71c49baa4da1e
Zenotope <zaak.kerstetter@gmail.com>
2022-08-04 10:33:05 -0600

    added note

--- src/App.js
+++ src/App.js
@@ -2,30 +2,50 @@ import logo from './logo.svg';
 import * as Tone from 'tone'
 import './App.css';
 
-// document.querySelector('button')?.addEventListener('click', async () => {
-// 	await Tone.start()
-// 	console.log('audio is ready')
-// })
 
+
+document.querySelector('button')?.addEventListener('click', async () => {
+	await Tone.start()
+	console.log('audio is ready')
+})
+		
 const sampler = new Tone.Sampler({
 	urls: {
-    A1: "A1.mp3",
+		C3: "C3.mp3",
+	},
+	baseUrl: "./src/piano/",
+	onload: () => {
+		sampler.triggerAttackRelease(["C3"], 0.5);
+	}
+}).toDestination();
+
+console.log(sampler)
+const sampler2 = new Tone.Sampler({
+	urls: {
+		A1: "A1.mp3",
 		A2: "A2.mp3",
-    
 	},
 	baseUrl: "https://tonejs.github.io/audio/casio/",
-	}).toDestination();
+	onload: () => {
+		sampler2.triggerAttackRelease(["C1", "E1", "G1", "B1"], 0.5);
+	}
+}).toDestination();
 
-  Tone.loaded().then(() => {
-    sampler.triggerAttackRelease(["C3", "Db3", "D3", "D3"], 0.5);
-  })
+  console.log(sampler)
+  console.log(sampler2)
+ 
+  // const sampler = new Tone.Sampler({
+  //   urls: {
+  //     A1: "A1.mp3",
+  //     A2: "A2.mp3",
+  //   },
+  //   baseUrl: "https://tonejs.github.io/audio/casio/",
+  //   onload: () => {
+  //     sampler.triggerAttackRelease(["C1", "E1", "G1", "B1"], 0.5);
+  //   }
+  // }).toDestination();
 
-  // "C3": "C3.mp3",
-  //   "Db3": "C#3.mp3",
-  //   "D3": "D3.mp3",
-  //   "Eb3": "D#3.aif",
-  //   "E3": "E3.aif",
-
+  // console.log(sampler2)
 
 function App() {
   return (
